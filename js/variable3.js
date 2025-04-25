d0ee9952985c4704f5c4af94546ad3aef7a2eba9
Your Name <you@example.com>
2025-04-25 15:06:17 +0900

    연산자까지

--- js/variable3.js
+++ js/variable3.js
@@ -4,7 +4,8 @@ let num1 = document.querySelector('#num1').value;
 let num2 = document.querySelector('#num2').value;
 num1 = parseInt(num1); //"30" ->30
 num2 = parseInt(num2); //"17"->17 parseInt를 넣어서 값을 숫자값으로 바꿔줌
-
+num1++
+++num1
 
 let result = num1++ + --num2; // "30" + "17" = "3017" ;  47
 console.log(result, num1, num2);
